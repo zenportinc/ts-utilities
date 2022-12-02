8b25c5bf1024c38267d6ead6cd9a2780a02c8b48
Carlos Labrador <carlos.labrador@zenport.io>
2022-12-02 21:10:59 +0900

    Added prettier and lint

--- src/index.ts
+++ src/index.ts
@@ -2,31 +2,29 @@ type NotAllowedSpecialCharacters = `${string}${'\\' | '//' | ':' | '*' | '?' | '
 type AllowedSpecialCharacters = `${string}${'_' | '-' | '%' | '+' | '!' | "'" | '{' | '}' | '~'}${string}`;
 
 const charactersMap = new Map<NotAllowedSpecialCharacters, AllowedSpecialCharacters>([
-    ['\\', '_'],
-    ['//', '-'],
-    [':', '%'],
-    ['*', '-'],
-    ['?', '!'],
-    ['"', "'"],
-    ['<', "{"],
-    ['>', "}"],
-    ['|', "~"],
-
-])
+  ['\\', '_'],
+  ['//', '-'],
+  [':', '%'],
+  ['*', '-'],
+  ['?', '!'],
+  ['"', "'"],
+  ['<', '{'],
+  ['>', '}'],
+  ['|', '~'],
+]);
 
 /**
  * Regex match rule:
  */
-const regex = /[\\\\/:*?"<>|]/g
-
+const regex = /[\\\\/:*?"<>|]/g;
 
 /**
  * Replacer
  * find a matching character and replace it with the map value
  */
-export const replacer = (char: string): string => charactersMap.get(char as NotAllowedSpecialCharacters) ?? ''
+export const replacer = (char: string): string => charactersMap.get(char as NotAllowedSpecialCharacters) ?? '';
 
-//================================UNIT TEST===========================================//
+// ================================UNIT TEST===========================================//
 // const testShipments = new Map<string, string>([
 //     ['my\\shipment\\to\\japan', 'my_shipment_to_japan'],
 //     ['11//24//2022', '11-24-2022'],
@@ -34,7 +32,6 @@ export const replacer = (char: string): string => charactersMap.get(char as NotA
 //     ['my*asterisk*shipment', 'my+asterisk+shipment'],
 //     ['where is it?', 'where is it!']
 // ])
-
 
 // testShipments.forEach((value, expected) => {
 //     const result = value.replace(regex, replacer)
