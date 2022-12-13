bb4dac6734bf18edb9b8a987a84a7c991e447291
Eliot Austin-Forbes <ejaustinforbes@gmail.com>
2022-12-13 13:57:53 +0000

    chore: function for mapping strings

--- src/index.ts
+++ src/index.ts
@@ -24,4 +24,8 @@ export const regex = /[\\\\/:*?"<>|]/g;
  */
 export const replacer = (char: string): string => charactersMap.get(char as NotAllowedSpecialCharacters) ?? '';
 
-
+/**
+ * parseSpecialCharactersWindowsFolder
+ * transform a string replacing values appropriately when matching a regex
+ */
+export const parseSpecialCharactersWindowsFolder = (str: string): string => str.replace(regex, replacer);
