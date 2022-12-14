084a88b3929286abadf8a0ee4c5dca023d23a6b8
Carlos Labrador <carlos.labrador@zenport.io>
2022-12-14 18:50:41 +0900

    feat: add test for slashes

--- src/__tests__/Characters.test.ts
+++ src/__tests__/Characters.test.ts
@@ -3,16 +3,22 @@ import { regex, replacer } from '../index'
 // ================================UNIT TEST===========================================//
 const testShipments = new Map<string, string>([
     ['my\\shipment\\to\\japan', 'my_shipment_to_japan'],
-    ['11//24//2022', '11-24-2022'],
+    ['11/24/2022', '11-24-2022'],
     ['hello:world', 'hello%world'],
     ['my*asterisk*shipment', 'my+asterisk+shipment'],
-    ['where is it?', 'where is it!']
+    ['where is it?', 'where is it!'],
+    ['22/22', '22-22'],
+    ['22//22', '22--22'],
+    ['22///22', '22---22'],
+    ['22\\22', '22_22'],
+    ['22\\\\22', '22__22'],
+    ['22\\\\\\22', '22___22'],
 ]);
 
 test('My Greeter', () => {
     testShipments.forEach((value,key) => {
-        const result =value.replace(regex,replacer)
-        // console.log(`assert ${value}`, result == value)
+        const result = key.replace(regex,replacer)
+        console.log(`assert ${value}`, result == value)
         expect(result).toBe(value)
     })
 });
